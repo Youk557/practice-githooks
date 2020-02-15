be3d3631bb7a50a8323d95ed373ef79c0f477f5b
youk-h <kazurou557@yahoo.co.jp>
2020-02-15 16:45:31 +0900

    change path to hooks directory

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 root=$(git rev-parse --show-superproject-working-tree)
 submodule=$(git rev-parse --show-toplevel)
 
-githooks=$submodule/githooks
+githooks=$submodule/hooks
 
 # configure to read our .gitconfig in .git/config
 git config -f $root/.git/config core.hooksPath $githooks
