a7b4db08a11e3a345aca9dbb48b85beedb7b84df
youk-h <kazurou557@yahoo.co.jp>
2020-02-15 17:07:00 +0900

    [refactor] rename script file

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-root=$(git rev-parse --show-superproject-working-tree)
-submodule=$(git rev-parse --show-toplevel)
-
-githooks=$submodule/hooks
-
-git config -f $root/.git/config core.hooksPath $githooks
